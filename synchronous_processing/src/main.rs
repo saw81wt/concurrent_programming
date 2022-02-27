cc3db3afeeb5e744f552a0e6c5c9cfa708ec7b31
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-27 10:24:21 +0900

    semaphore

--- synchronous_processing/src/main.rs
+++ synchronous_processing/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex, Condvar, Barrier};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use std::env;
 
@@ -30,6 +31,38 @@ fn parent(p: Arc<(Mutex<bool>, Condvar)>) {
     *started = true;
     cvar.notify_all();
     println!("parent");
+}
+
+pub struct Semaphore {
+    mutex: Mutex<isize>,
+    cond: Condvar,
+    max: isize,
+}
+
+impl Semaphore {
+    pub fn new(max: isize) -> Self {
+        Semaphore {
+            mutex: Mutex::new(0),
+            cond: Condvar::new(),
+            max,
+        }
+    }
+
+    pub fn wait(&self) {
+        let mut cnt = self.mutex.lock().unwrap();
+        while *cnt >= self.max {
+            cnt = self.cond.wait(cnt).unwrap();
+        }
+        *cnt += 1;
+    }
+
+    pub fn post(&self) {
+        let mut cnt = self.mutex.lock().unwrap();
+        *cnt -= 1;
+        if *cnt <= self.max {
+            self.cond.notify_one();
+        }
+    }
 }
 
 fn mutex_example() {
@@ -67,6 +100,7 @@ fn condition_example() {
 }
 
 fn barrier_example() {
+    // 3.8.4 barrier
     let mut v = Vec::new();
 
     let barrier = Arc::new(Barrier::new(10));
@@ -85,8 +119,41 @@ fn barrier_example() {
     }
 }
 
+fn semaphore_example() {
+    const NUM_LOOP: usize = 10;
+    const NUM_THREAD: usize = 8;
+    const SEM_NUM: isize = 4;
+
+    static mut CNT: AtomicUsize = AtomicUsize::new(0);
+
+    let mut v = Vec::new();
+    let sem = Arc::new(Semaphore::new(SEM_NUM));
+
+    for i in 0..NUM_THREAD {
+        let s = sem.clone();
+        let t = std::thread::spawn(move || {
+            for _ in 0..NUM_LOOP {
+                s.wait();
+
+                unsafe { CNT.fetch_add(1, Ordering::SeqCst) };
+                let n = unsafe { CNT.load(Ordering::SeqCst) };
+                println!("semaphore: i = {}, CNT = {}", i, n);
+                assert!((n as isize) <= SEM_NUM);
+                unsafe { CNT.fetch_sub(1, Ordering::SeqCst) };
+                
+                s.post();
+            }
+        });
+        v.push(t);
+    }
+
+    for t in v {
+        t.join().unwrap();
+    }
+}
+
 fn help() {
-    println!("Requires one argument. (mutex|condition)");
+    println!("Requires one argument. (mutex|condition|barrier)");
 }
 
 fn main() {
@@ -98,6 +165,7 @@ fn main() {
                 "mutex" => mutex_example(),
                 "condition" => condition_example(),
                 "barrier" => barrier_example(),
+                "semaphore" => semaphore_example(),
                 _ => help()
             }
         },
